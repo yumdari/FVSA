52340dd8a879e497aa5519fc2ed0f7e2c2ff45d9
yumdari <epqmfzkdlwj@naver.com>
2023-11-05 22:27:44 +0900

    [ref: #5] switch문을 활용한 전방 차량 인식 구현

--- FVSA/FVSA.ino
+++ FVSA/FVSA.ino
@@ -2,23 +2,30 @@
 /*Alerts the driver when the vehicle in front has started to move*/
 /*2023년도 ICT이노베이션스퀘어 확산 사업 - 아두이노 기반 모빌리티 IoT 과정*/
 //  Changelog:
+//  23.11.05 - front car detecting logic
 //  23.10.30 - create varibles for detecting car
 //  23.10.29 - create project
 //           - ultrasonic sensor setting
 
 #define TRIG 5
 #define ECHO 6
+#define ULTRA_DELAY 1000
+
 #define BUZZER 11
 
 enum carState {
-  STOP = 0,
+  INVALID = 0,
+  DETECTED,
+  STOP,
   DEPART
 };
 
 typedef struct {
-  uint8_t frontCarState;
-  uint8_t myCarState;
+  uint8_t stopStart;
+  uint8_t preStopStart;
 }CarState;
+
+unsigned long curTime = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,10 +35,22 @@ void setup() {
   pinMode(ECHO, INPUT);   //초음파 수신부를 입력 설정
   
   pinMode(BUZZER, OUTPUT);
+
+  curTime = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  CarState myCarState;
+  CarState frontCarState;
+
+  memset(&myCarState, 0x00, sizeof(myCarState));
+  memset(&frontCarState, 0x00, sizeof(frontCarState));
+
+
+  static uint8_t alert = 0;
+  
   digitalWrite(TRIG, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG, HIGH); 
@@ -40,11 +59,73 @@ void loop() {
 
   unsigned long duration = pulseIn(ECHO, HIGH); // ECHO 핀이 HIGH 상태가 될 때까지 시간 측정
 
-  float distance = ((float)(340*duration) / 10000) / 2; // 초음파는 1초당 340m를 이동
-                                          // 따라서, 초음파의 이동 거리 = duration(왕복에 걸린시간)*340 / 1000 / 2
+  static float distance = 0;
+  if((duration >= 117.6471)&&(duration <= 23529.41176))
+    distance = ((float)(340*duration) / 10000) / 2; // 초음파는 1초당 340m를 이동
+                                                        // 따라서, 초음파의 이동 거리 = duration(왕복에 걸린시간)*340 / 1000 / 2
+  static float preDistance = 0;
+  
+  if(preDistance == 0)
+  {
+    preDistance = distance;
+    Serial.println("preDistance init");
+  }
+
+  if(preDistance != 0)
+  {
+    if ((distance>(preDistance*10)) || (distance < (preDistance/15))) // 초음파 센서 측정값 튀는걸 필터링
+      {
+        if((distance>(preDistance*5)))
+          Serial.println("filtering *");
+        else 
+          Serial.println("filtering /");
+       distance = preDistance;
+      }
+  }
 
   Serial.print(distance);   // 측정된 거리 값를 시리얼 모니터에 출력
   Serial.println("cm");
+
+  switch(frontCarState.stopStart)
+  {
+    case INVALID :
+    if(distance <= 30)
+    {
+      frontCarState.stopStart = DETECTED;
+      Serial.println("front car detected");
+    }
+    break;
+    
+    case DETECTED : 
+    if(millis() > curTime + 3000)
+    {
+      if(distance <= 30)
+      {
+        frontCarState.stopStart = STOP;
+        Serial.println("front car stopped");
+        curTime = millis();
+      }
+    }
+    break;
   
-  delay(1000);
+    case STOP :
+    if(millis() > curTime + 3000)
+    {
+      if(distance > 30)
+      {
+        frontCarState.stopStart = DEPART;
+        Serial.println("front car departed");
+        curTime = millis();
+      }
+    }
+    break;
+    
+    case DEPART : 
+     //digitalWrite(13, HIGH);
+    Serial.println("alerted");
+    break;
+  }
+
+  delay(ULTRA_DELAY);
+  preDistance  = distance;
 }
